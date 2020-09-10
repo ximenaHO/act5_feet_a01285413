26e47517b6832856ec613271ee3a60188f1bdbae
ximenaHO <70345025+ximenaHO@users.noreply.github.com>
2020-09-10 14:37:14 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(feet)
+    basic.showString("feet")
+    basic.showString("=")
+    yards = feet / 3
+    basic.showNumber(yards)
+    basic.showString("Yards")
+})
+let yards = 0
+let feet = 0
+feet = randint(1, 100)
+basic.forever(function () {
+	
+})
